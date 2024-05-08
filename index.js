3c4a385d9410b2ede7417ed0e5bff1e310a04dc9
congsonminh196 <taoufikboypun0suy@hotmail.com>
2024-05-09 01:25:05 +0700

    start project

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-function isValidSudoku(board) {
-  for (let i = 0; i < 9; i++) {
-    const row = new Set();
-    const col = new Set();
-    const box = new Set();
-    for (let j = 0; j < 9; j++) {
-      const rowVal = board[i][j];
-      const colVal = board[j][i];
-      const boxVal =
-        board[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)];
-      if (rowVal !== ".") {
-        if (row.has(rowVal)) return false;
-        row.add(rowVal);
-      }
-      if (colVal !== ".") {
-        if (col.has(colVal)) return false;
-        col.add(colVal);
-      }
-      if (boxVal !== ".") {
-        if (box.has(boxVal)) return false;
-        box.add(boxVal);
-      }
+function decodeString(s) {
+  const stack = [];
+  for (const char of s) {
+    if (char !== "]") stack.push(char);
+    else {
+      let str = "";
+      while (stack.length && stack[stack.length - 1] !== "[")
+        str = stack.pop() + str;
+      stack.pop();
+      let num = "";
+      while (stack.length && !isNaN(stack[stack.length - 1]))
+        num = stack.pop() + num;
+      stack.push(str.repeat(parseInt(num)));
     }
   }
-  return true;
+  return stack.join("");
 }
